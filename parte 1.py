fd3047ce0c5fd39d6927c7935c9f9787f3ed1d4a
jodaruve <jose.rueda.2019@upb.edu.co>
2019-03-29 10:59:46 -0500

    Punto 1 1.1

--- parte 1.py
+++ parte 1.py
@@ -1,13 +1,23 @@
 ## parte 1
-def a_power_b():
+def a_power_b(a,b):
   prod=1
   for i in range (1,b+1):
     prod=prod*a
-  print(prod)
+  
+  return prod
   
   
-print("Este programa calculara el elevado de los numeros ingresados.")  
-a=int(input("Por favor ingrese un numero "))
-b=int(input("Por favor ingrese otro numero "))
-print("El resultado de elevar", a, "estre", b, "es:")
-a_power_b()
+print("Este programa calculara el elevado de los numeros ingresados. Cuando no desee ingresar mas numeros por favor digite 0")  
+
+while True:
+
+  a=int(input("Por favor ingrese un numero "))
+  
+  if a <= 0:
+    break
+  
+  b=int(input("Por favor ingrese otro numero "))
+
+  
+  res = a_power_b(a,b)
+  print("El resultado de elevar", a, "entre", b, "es:", res)
