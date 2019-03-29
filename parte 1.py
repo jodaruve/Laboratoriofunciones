5c6a914d3d95d8e407c0255d45d380a6baf08564
jodaruve <jose.rueda.2019@upb.edu.co>
2019-03-29 11:34:23 -0500

    Punto 1 1.2

--- parte 1.py
+++ parte 1.py
@@ -1,7 +1,12 @@
 ## parte 1
 def a_power_b(a,b):
   prod=1
+  
   for i in range (1,b+1):
+    
+    if i>1000:
+      raise( Exception('TOOOO BIG') )
+    
     prod=prod*a
   
   return prod
@@ -9,15 +14,19 @@ def a_power_b(a,b):
   
 print("Este programa calculara el elevado de los numeros ingresados. Cuando no desee ingresar mas numeros por favor digite 0")  
 
+
 while True:
+  try:
+      
+    a=int(input("Por favor ingrese un numero "))
+    
+    if a <= 0:
+      break
+    
+    b=int(input("Por favor ingrese otro numero "))
 
-  a=int(input("Por favor ingrese un numero "))
-  
-  if a <= 0:
-    break
-  
-  b=int(input("Por favor ingrese otro numero "))
+    res = a_power_b(a,b)
+    print("El resultado de elevar", a, "entre", b, "es:", res)
 
-  
-  res = a_power_b(a,b)
-  print("El resultado de elevar", a, "entre", b, "es:", res)
+  except: 
+    print("Ha ocurrido un error. :(")
