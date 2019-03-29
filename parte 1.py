683226e610433d2a7e6fbf244cfb5b304977af34
jodaruve <jose.rueda.2019@upb.edu.co>
2019-03-29 10:39:10 -0500

    Parte 1.0

---
+++ parte 1.py
@@ -0,0 +1,12 @@
+## Parte 1
+print("Este programa calculara el elevado de los numeros ingresados")
+a=int(input("Por favor ingrese un numero "))
+b=int(input("Por favor ingrese otro numero "))
+def a_power_b():
+  prod=1
+  for i in range (1,b+1):
+    prod=prod*a
+  print(prod)
+    
+print("El resultado de elevar", a, "estre", b, "es:")
+a_power_b()
