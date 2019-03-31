ead212e136afb339d834420c891c2ae5b3829ddd
jodaruve <jose.rueda.2019@upb.edu.co>
2019-03-30 22:36:54 -0500

    Parte 2 1.3 Casi funciona

--- parte 2.py
+++ parte 2.py
@@ -1,5 +1,6 @@
 def is_prime(a):
   i=0
+  primo=0
   for n in range(1,a+1):
     if a%n==0:
      i=i+1
@@ -9,11 +10,13 @@ def is_prime(a):
   else:
     si="1"
     print(si)
+    primo=primo+1
+    print("El total de primos fue:", primo)
+    
 
 print("-Este programa calculara si el numero ingresado es primo o no. Si el mensaje devuelto es 1 significa que el numero es primo, si devuelve 0 significa que el numero no es primo, si devuelve -1 significa que ha ocurrido un error. Del mismo modo cuando no desee ingresar mas numeros digite -00")
 print("")
 try:
-  
   a=int(input("Por favor ingrese un numero "))
   while a!=-00:
       if a !=0 and a >1:
@@ -21,7 +24,7 @@ try:
         a=int(input("Por favor ingrese un numero "))
       else:
         break 
-        print("Fin del programa")
+      
 except:
   error="-1"
   print(error)
